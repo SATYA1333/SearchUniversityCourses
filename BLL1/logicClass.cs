0af6fd6986d3afb97d262203f5fc53c783208c16
SATYA1333 <95492608+SATYA1333@users.noreply.github.com>
2021-12-03 23:22:11 +0530

    Add files via upload

---
+++ BLL1/logicClass.cs
@@ -0,0 +1,48 @@
+﻿using BOL1;
+using DAL1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL1
+{
+    public class logicClass
+    {
+        private dbCoursesClass objCourses;
+        public logicClass()
+        {
+            objCourses = new dbCoursesClass();
+        }
+
+        public List<UniversityCouseViewModel> GetUniversityCousesData()
+        {
+            return objCourses.GetUniversityCousesData();
+        }
+        public List<UniversityCouseViewModel> SearchUniversityCousesData(string option,string search)
+        {
+            var data= objCourses.GetUniversityCousesData();
+            List<UniversityCouseViewModel> obj = new List<UniversityCouseViewModel>();
+
+            if (option== "Country")
+            {
+                return data.Where(x => x.country.ToUpper().Contains(search.ToUpper()) || search == null).ToList();
+            }
+            else if (option == "courseName")
+            {
+                return data.Where(x => x.courseName.ToUpper().Contains(search.ToUpper()) || search == null).ToList();
+            }
+            else if (option == "minimum_gpa")
+            {
+                return data.Where(x => x.minimum_gpa.ToString().Contains(search) || search == null).ToList();
+            }
+            else if(option == "minimum_gre_score") 
+            {
+                return data.Where(x => x.minimum_gre_score.ToString().Contains(search) || search == null).ToList();
+            }
+            return data;
+        }
+
+    }
+}
---
+++ DAL1/dbCoursesClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BOL1;
+
+namespace DAL1
+{
+   public class dbCoursesClass
+    {
+        private UniversityCoursesEntity db;
+        public dbCoursesClass()
+        {
+            db = new UniversityCoursesEntity();
+        }
+        public List<Cours> getAllCoursesData()
+        {
+            return db.Courses.ToList();
+        }
+        public List<UniversityCouseViewModel> GetUniversityCousesData()
+        {
+            List<UniversityCouseViewModel> couseViewModels = new List<UniversityCouseViewModel>();
+            var details = (from University in db.Universities
+                           join Cours in db.Courses on University.id equals Cours.university_id
+                           select new { universityname = University.name, University.country, University.minimum_gpa, University.minimum_gre_score, couseName = Cours.name }).ToList();
+
+            foreach (var item in details)
+            {
+                UniversityCouseViewModel obj = new UniversityCouseViewModel();
+                obj.courseName = item.couseName;
+                obj.country = item.country;
+                obj.universityName = item.universityname;
+                obj.minimum_gpa = item.minimum_gpa;
+                obj.minimum_gre_score = item.minimum_gre_score;
+                couseViewModels.Add(obj);
+
+
+            }
+            return couseViewModels;
+        }
+    }
+}
