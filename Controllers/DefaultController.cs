d48263e6c9934202da4e1f08bc204516fd81a8aa
SATYA1333 <95492608+SATYA1333@users.noreply.github.com>
2021-12-07 13:30:15 +0530

    Add files via upload

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,97 @@
+﻿using BLL1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UniversityCourses.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private logicClass logicClass;
+        public DefaultController()
+        {
+            logicClass = new logicClass();
+        }
+        // GET: Default
+        public ActionResult Index(string option, string search)
+        {
+            var data = logicClass.SearchUniversityCousesData(option, search);
+            return View(data);
+        }
+
+        // GET: Default/Details/5
+        public ActionResult Index1(string option, string search)
+        {
+            var data = logicClass.SearchUniversityCousesData( option, search);
+            return View(data);
+        }
+
+        // GET: Default/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Default/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Default/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Default/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Default/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Default/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
